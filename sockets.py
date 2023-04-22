945a94cfb8daf92ed3679937d7e110ec5e31b2a2
TamirlanMakhov <makhov.tamirlan@gmail.com>
2023-04-23 02:23:21 +0300

    non-blocking sockets with selectors and event loop

--- sockets.py
+++ sockets.py
@@ -1,27 +1,35 @@
+import selectors
 import socket
+from selectors import SelectorKey
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+selector = selectors.DefaultSelector()
+
+server_socket = socket.socket()
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-address = ('127.0.0.1', 8000)
-server_socket.bind(address)
-
+server_address = ('127.0.0.1', 8000)
+server_socket.setblocking(False)
+server_socket.bind(server_address)
 server_socket.listen()
 
-try:
-    connection, client_address = server_socket.accept()
-    print(f'got connection request from {client_address}')
+selector.register(server_socket, selectors.EVENT_READ)
 
-    buffer = b''
+while True:
+    events: list[tuple[SelectorKey, int]] = selector.select(timeout=1)  # A
 
-    while buffer[-2:] != b'\r\n':
-        data = connection.recv(2)
-        if not data:
-            break
+    if len(events) == 0:  # B
+        print('No events, waiting a bit more!')
+
+    for event, _ in events:
+        event_socket = event.fileobj  # C
+
+        if event_socket == server_socket:  # D
+            connection, address = server_socket.accept()
+            connection.setblocking(False)
+            print(f"I got a connection from {address}")
+            selector.register(connection, selectors.EVENT_READ)  # E
         else:
-            print(f'got data {data}')
-            buffer += data
-        print(f'all data: {buffer}')
-        connection.sendall(buffer)
-finally:
-    server_socket.close()
+            data = event_socket.recv(1024)  # F
+            print(f"I got some data: {data}")
+            event_socket.send(data)
